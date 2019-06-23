2a4e160aca10d3ec3e47ff4c38eb8d171f7d2bab
Martin Prospers <martinprospers1@gmail.com>
2019-06-23 18:55:44 +0000

    Successfully established connection of login with authentication to front end. also created table for Services to hold all of the services info as a catalog of what Still offers its clients.

--- src/main.py
+++ src/main.py
@@ -7,7 +7,10 @@ from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
-from models import db, Client
+from models import db, Client, Orders, Service_catalog
+from flask_jwt_simple import (
+    JWTManager, jwt_required, create_jwt, get_jwt_identity
+)
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
@@ -15,6 +18,42 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
+
+
+# *************************************************************************************
+# ******************************   JWT/ LOGIN METHOD   ********************************
+# *************************************************************************************
+app.config['JWT_SECRET_KEY'] = '$8455&843AM735I33!'
+jwt = JWTManager(app)
+
+#                 # Provide a method to create access tokens.
+#                 # The create_jwt() function is used to actually generate the token
+@app.route('/login', methods=['POST'])
+def login():
+    if not request.is_json:
+        return jsonify({"msg": "Missing JSON in request"}), 400
+
+    params = request.get_json()
+    email = params.get('email', None)
+    password = params.get('password', None)
+
+    if not email:
+        return jsonify({"msg": "Missing email parameter"}), 400
+    if not password:
+        return jsonify({"msg": "Missing password parameter"}), 400
+
+    client_check = Client.query.filter_by(email=email, password=password).first()
+    if client_check == None:
+
+        return jsonify({"msg": "Bad email or password"}), 401
+
+#         # Identity can be any data that is json serializable
+    ret = {'jwt': create_jwt(identity=email)}
+    return jsonify(ret), 200
+# *************************************************************************************
+
+
+
 
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
@@ -24,13 +63,22 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+
+# *************************************************************************************
+# *******************************   CLIENT METHODS   **********************************
+# *************************************************************************************
+
+
+# *******************************  CLIENT (GENERAL)  **********************************
+# *************************************************************************************
 @app.route('/client', methods=['POST', 'GET'])
+# @jwt_required
 def handle_client():
     """
     Create client and retrieve all clients
     """
 
-    # POST request
+# *******************   POST REQUEST    *******************
     if request.method == 'POST':
         body = request.get_json()
 
@@ -43,58 +91,270 @@ def handle_client():
         if 'password' not in body:
             raise APIException('You need to specify the password', status_code=400)
 
-
-        user1 = Client(name=body['name'], email=body['email'], password=body['password'])
-        db.session.add(user1)
+        client1 = Client(name=body['name'], email=body['email'], password=body['password'])
+        db.session.add(client1)
         db.session.commit()
-        return "ok", 200
+        return jsonify(client1.serialize()), 200
 
-    # GET request
+# *******************   GET REQUEST    *******************
     if request.method == 'GET':
-        all_people = Client.query.all()
-        all_people = list(map(lambda x: x.serialize(), all_people))
-        return jsonify(all_people), 200
+        all_clients= Client.query.all()
+        all_clients = list(map(lambda x: x.serialize(), all_clients))
+        return jsonify(all_clients), 200
 
     return "Invalid Method", 404
 
 
+# **************************  INDIVIDUAL/SPECIFIC CLIENT  *****************************
+# *************************************************************************************
 @app.route('/client/<int:client_id>', methods=['PUT', 'GET', 'DELETE'])
+@jwt_required
 def get_single_client(client_id):
     """
     Single client
     """
 
-    # PUT request
+# *******************   PUT REQUEST    *******************
     if request.method == 'PUT':
         body = request.get_json()
         if body is None:
             raise APIException("You need to specify the request body as a json object", status_code=400)
 
-        user1 = Client.query.get(client_id)
-        if user1 is None:
-            raise APIException('User not found', status_code=404)
+        client1 = Client.query.get(client_id)
+        if client1 is None:
+            raise APIException('Client not found', status_code=404)
 
-        if "username" in body:
-            user1.username = body["username"]
         if "email" in body:
-            user1.email = body["email"]
+            client1.email = body["email"]
+        if "password" in body:
+            client1.password = body["password"]
+
         db.session.commit()
+        return jsonify(client1.serialize()), 200
 
-        return jsonify(user1.serialize()), 200
-
-    # GET request
+# *******************   GET REQUEST    *******************
     if request.method == 'GET':
-        user1 = Client.query.get(client_id)
-        if user1 is None:
-            raise APIException('User not found', status_code=404)
-        return jsonify(user1.serialize()), 200
+        client1 = Client.query.get(client_id)
+        if client1 is None:
+            raise APIException('Client not found', status_code=404)
 
-    # DELETE request
+        return jsonify(client1.serialize()), 200
+
+# *******************   DELETE REQUEST    *******************
     if request.method == 'DELETE':
-        user1 = Client.query.get(client_id)
-        if user1 is None:
-            raise APIException('User not found', status_code=404)
-        db.session.delete(user1)
+        client1 = Client.query.get(client_id)
+        if client1 is None:
+            raise APIException('Client not found', status_code=404)
+
+        db.session.delete(client1)
+        db.session.commit()
+        return "ok", 200
+
+    return "Invalid Method", 404
+# *************************************************************************************
+
+
+
+
+# *************************************************************************************
+# *******************************   ORDERS METHODS   **********************************
+# *************************************************************************************
+
+
+# *******************************  ORDERS (GENERAL)  **********************************
+# *************************************************************************************#
+@app.route('/orders', methods=['POST', 'GET'])
+def handle_orders():
+    """
+    Create orders and retrieve all orders
+    """
+
+# *******************   POST REQUEST    *******************
+    if request.method == 'POST':
+        body = request.get_json()
+
+        if body is None:
+            raise APIException("You need to specify the request body as a json object",
+                               status_code=400)
+
+        if 'client_email' not in body:
+            raise APIException('You need to specify client_email', status_code=400)
+        # if 'total' not in body:
+        #     raise APIException('You need to specify the total', status_code=400)
+        # if 'order_number' not in body:
+        #     raise APIException('You need to specify the order number', status_code=400)
+        # if 'date_created' not in body:
+        #     raise APIException('You need to specify the date created', status_code=400)
+        # if 'assigned_consultant' not in body:
+        #     raise APIException('You need to specify the assigned_consultant', status_code=400)
+
+        order1 = Orders(client_email=body['client_email'])
+                            #    total=body['total'],
+                        # order_number=body['order_number'],
+                        # date_created=body['date_created'],
+        db.session.add(order1)
+        db.session.commit()
+        return "ok", 200
+
+# *******************   GET REQUEST    *******************
+    if request.method == 'GET':
+        all_orders = Orders.query.all()
+        all_orders = list(map(lambda x: x.serialize(), all_orders))
+        return jsonify(all_orders), 200
+
+    return "Invalid Method", 404
+
+
+
+# **************************  INDIVIDUAL/SPECIFIC ORDER  ******************************
+# *************************************************************************************
+@app.route('/orders/<int:id>', methods=['PUT', 'GET', 'DELETE'])
+def get_single_order(id):
+    """
+    Single order
+    """
+
+# *******************   PUT REQUEST    *******************
+    if request.method == 'PUT':
+        body = request.get_json()
+        if body is None:
+            raise APIException("You need to specify the request body as a json object", status_code=400)
+
+        order1 = Orders.query.get(id)
+        if order1 is None:
+            raise APIException('Order not found', status_code=404)
+
+        if "client_email" in body:
+            order1.client_email = body["client_email"]
+        if "payment_status" in body:
+            order1.payment_status = body["payment_status"]
+        if "meeting_type" in body:
+            order1.meeting_type = body["meeting_type"]
+        if "assigned_consultant" in body:
+            order1.assigned_consultant = body["assigned_consultant"]
+        if "order_status" in body:
+            order1.order_status = body["order_status"]
+        if "order_notes" in body:
+            order1.order_notes = body["order_notes"]
+        if "order_issues" in body:
+            order1.order_issues = body["order_issues"]
+        if "date_completed" in body:
+            order1.date_completed = body["date_completed"]
+        if "total" in body:
+            order1.total = body["total"]
+
+        db.session.commit()
+        return jsonify(order1.serialize()), 200
+
+# *******************   GET REQUEST    *******************
+    if request.method == 'GET':
+        order1 = Orders.query.get(id)
+        if order1 is None:
+            raise APIException('Order not found', status_code=404)
+
+        return jsonify(order1.serialize()), 200
+
+# *******************   DELETE REQUEST    *******************
+    if request.method == 'DELETE':
+        order1 = Orders.query.get(id)
+        if order1 is None:
+            raise APIException('Order not found', status_code=404)
+
+        db.session.delete(order1)
+        db.session.commit()
+        return "ok", 200
+
+    return "Invalid Method", 404
+
+
+
+
+# *************************************************************************************
+# *******************************  SERVICES METHODS   *********************************
+# *************************************************************************************
+
+# ******************************   SERVICES (GENERAL)   *******************************
+# *************************************************************************************
+@app.route('/services', methods=['POST', 'GET'])
+def handle_service_catalog():
+    """
+    Retrieve all Services
+    """
+
+# *******************   POST REQUEST    *******************
+    if request.method == 'POST':
+        body = request.get_json()
+
+        if body is None:
+            raise APIException("You need to specify the request body as a json object",
+                               status_code=400)
+
+        if 'service' not in body:
+            raise APIException('You need to specify the service', status_code=400)
+
+
+        service1 = Service_catalog(service=body['service'])
+        db.session.add(service1)
+        db.session.commit()
+        return "ok", 200
+
+# *******************   GET REQUEST    *******************
+    if request.method == 'GET':
+        all_services = Service_catalog.query.all()
+        all_services = list(map(lambda x: x.serialize(), all_services))
+        return jsonify(all_services), 200
+
+    return "Invalid Method", 404
+
+
+
+# **************************  INDIVIDUAL/SPECIFIC SERVICE  ******************************
+# *************************************************************************************
+@app.route('/services/<int:id>', methods=['PUT', 'GET', 'DELETE'])
+def get_single_service(id):
+    """
+    Single service
+    """
+
+# *******************   PUT REQUEST    *******************
+    if request.method == 'PUT':
+        body = request.get_json()
+        if body is None:
+            raise APIException("You need to specify the request body as a json object", status_code=400)
+
+        service1 = Service_catalog.query.get(id)
+        if service1 is None:
+            raise APIException('Order not found', status_code=404)
+
+        if "assigned_consultant" in body:
+            service1.assigned_consultant = body["assigned_consultant"]
+        if "service" in body:
+            service1.service = body["service"]
+        if "description" in body:
+            service1.description = body["description"]
+        if "price" in body:
+            service1.price = body["price"]
+        if "service_type" in body:
+            service1.service_type = body["service_type"]
+        if "package" in body:
+            service1.package = body["package"]
+
+        db.session.commit()
+        return jsonify(service1.serialize()), 200
+
+# *******************   GET REQUEST    *******************
+    if request.method == 'GET':
+        service1 = Service_catalog.query.get(id)
+        if service1 is None:
+            raise APIException('Service not found', status_code=404)
+        return jsonify(service1.serialize()), 200
+
+# *******************   DELETE REQUEST    *******************
+    if request.method == 'DELETE':
+        service1 = Service_catalog.query.get(id)
+        if service1 is None:
+            raise APIException('Order not found', status_code=404)
+        db.session.delete(service1)
         db.session.commit()
         return "ok", 200
 
--- src/models.py
+++ src/models.py
@@ -1,21 +1,109 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Table, Column, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+
 
 db = SQLAlchemy()
 
-class Client(db.Model):
-    client_id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), unique=False, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(120), unique=True, nullable=False)
 
+
+
+# *************************        CLIENT TABLE         *******************************
+# *************************************************************************************
+class Client(db.Model):
+    __tablename__ = 'client'
+    client_id = db.Column(db.Integer, primary_key=True, default=None)
+    name = db.Column(db.String(80), unique=False, nullable=False)
+    email = db.Column(db.String(120), unique=True, default=None, nullable=False)
+    password = db.Column(db.String(120), unique=True, default=None, nullable=False)
+
+    orders_id = db.Column(Integer, ForeignKey('orders.id'))
+    service_catalog_id = db.Column(Integer, ForeignKey('service_catalog.id'))
+    # orders = relationship("Orders")
+    # orders = relationship("Orders", back_populates="Client")
 
     def __repr__(self):
         return '<Client %r>' % self.email
 
     def serialize(self):
         return {
-            "Client ID": self.client_id,
+            "client_id": self.client_id,
             "name": self.name,
             "email": self.email,
             "password": self.password
+        }
+
+
+
+
+# *************************        ORDERS TABLE         *******************************
+# *************************************************************************************
+class Orders(db.Model):
+    __tablename__ = 'orders'
+    id = db.Column(db.Integer, primary_key=True)
+    date_created = db.Column(db.DateTime, nullable=True, default=None)
+    client_email = db.Column(db.String(120), nullable=True, default=None)
+    meeting_type = db.Column(db.String(120), nullable=True, default=None)
+    discount = db.Column(db.Integer, nullable=True, default=None)
+    payment_type = db.Column(db.String(120), nullable=True, default=None)
+    payment_status = db.Column(db.String(120), nullable=True, default=None)
+    order_status = db.Column(db.String(20), default = 'Open', nullable=True)
+    order_notes = db.Column(db.String(5000), nullable=True, default=None)
+    order_issues = db.Column(db.String(5000), nullable=True, default=None)
+    date_completed = db.Column(db.DateTime, nullable=True, default=None)
+    total = db.Column(db.Integer, nullable=True, default=None)
+    # order_number = db.Column(db.Integer, nullable=True, default=None)
+    # selected_services = db.Column(db.String(5000), nullable=True, default=None)
+    # assigned_consultant = db.Column(db.String(120), nullable=True, default=None)
+
+    # consultants = relationship('consultants')
+    # services = relationship('services')
+    service_catalog_id = db.Column(Integer, ForeignKey('service_catalog.id'))
+    client_id = db.Column(Integer, ForeignKey('client.client_id'))
+
+    def __repr__(self):
+        return '<Orders %r>' % self.id
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "date_created": self.date_created,
+            "order_status": self.order_status,
+            "total": self.total
+            # "order_number": self.order_number,
+            # "assigned_consultant": self.assigned_consultant,
+            # "selected_services": self.selected_services,
+        }
+
+
+
+
+# ************************        SERVICES TABLE         ******************************
+# *************************************************************************************
+class Service_catalog (db.Model):
+    __tablename__ = 'service_catalog'
+    id = db.Column(db.Integer, primary_key=True)
+    assigned_consultant = db.Column(db.String(120), nullable=True, default=None)
+    service = db.Column(db.String(120), nullable=True, default=None)
+    description = db.Column(db.String(5000), nullable=True, default=None)
+    price = db.Column(db.Integer, nullable=True, default=None)
+
+# Alejandro suggested making "service_type" (to specify if its from "Strategy", "Identity", or "Marketing") an Enum; but I couldn't figure out the proper syntax for it- even after googling it.
+    service_type = db.Column(db.String(5000), nullable=True, default=None)
+# Alejandro suggested making "package" a boolean; but I couldn't figure out how to write it, regarding syntax - even after googling it.
+    package = db.Column(db.String(5000), nullable=True, default=None)
+
+    orders_id = db.Column(Integer, ForeignKey('orders.id'))
+    client_id = db.Column(Integer, ForeignKey('client.client_id'))
+
+    def __repr__(self):
+        return '<service_catalog %r>' % self.service
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "service": self.service,
+            "assigned_consultant": self.assigned_consultant,
+            "service_type": self.service_type,
+            "price": self.price
         }
